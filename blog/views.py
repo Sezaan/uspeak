e67260e2c4129d5724bbefc349646c1143282631
root <root@Sezan.localdomain>
2020-08-10 07:45:30 +0600

    Added gemeric view

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
 
-def post_list(request):
-	object_list = Post.objects_published.all()
-	paginator = Paginator(object_list, 3)
-	page = request.GET.get('page')
-	try:
-		posts = paginator.page(page)
-	except PageNotAnInteger:
-		posts = paginator.page(1)
-	except EmptyPage:
-		posts = paginator.page(paginator.num_pages)
-
-	return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+class PostListView(ListView):
+	queryset = Post.objects_published.all()
+	context_object_name = 'posts'
+	paginate_by = 3
+	template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
@@ -22,3 +16,13 @@ def post_detail(request, year, month, day, post):
 								   publish__month=month,
 								   publish__day=day)
 	return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+	post = get_object_or_404(Post, id=post_id, status='published')
+	if(request.method == 'POST'):
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+	else:
+		form = EmailPostForm()
+	return render(request, 'blog/post/share.html', {'post': post, 'form': form})
