8b083fed3336a071309077e75ccc56e93e534646
root <root@Sezan.localdomain>
2020-08-17 17:06:21 +0600

    Added comment for users

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
+
 
 class PublishedManager(models.Manager):
 	def get_queryset(self):
 		return super(PublishedManager, self).get_queryset()\
 											.filter(status='published')
+
 
 class Post(models.Model):
 	STATUS_CHOICES = (
@@ -23,6 +26,7 @@ class Post(models.Model):
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 	objects = models.Manager()
 	objects_published = PublishedManager()
+	tags = TaggableManager()
 
 	class Meta:
 		ordering = ('-publish',)
@@ -36,3 +40,18 @@ class Post(models.Model):
 					  		self.publish.month,
 					  		self.publish.day,
 					  		self.slug])
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+	name = models.CharField(max_length=80)
+	email = models.EmailField()
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	active = models.BooleanField(default=True)
+
+	class Meta:
+		ordering = ('created',)
+
+	def __str__(self):
+		return f'Comment by {self.name} on {self.post}'
--- uspeak/settings.py
+++ uspeak/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # adding apps
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
